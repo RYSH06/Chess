dde154c1b5d0e0d37392d7a7574c2253e0f91e5f
RYSH06 <Raphael.yeom.020222@gmail.com>
2026-01-30 00:16:10 +0900

    Hashkey made for each board squares

--- vice.c
+++ vice.c
@@ -1,34 +1,12 @@
+// vice.c
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "defs.h"
 
 int main() {
 
   AllInit();
-
-  U64 playedBitBoard = 0ULL;
-
-  playedBitBoard |= (1ULL << SQ64(D2));
-  playedBitBoard |= (1ULL << SQ64(D3));
-  playedBitBoard |= (1ULL << SQ64(D4));
-
-  printf("\n");
-  PrintBitBoard(playedBitBoard);
-
-  int count = CNT(playedBitBoard);
-
-  printf("Count:%d\n", count);
-
-  int index = POP(&playedBitBoard);
-  printf("index:%d\n", index);
-  PrintBitBoard(playedBitBoard);
-  count = CNT(playedBitBoard);
-  printf("Count:%d\n", count);
-  
-  int sq64 = 0;
-  while (playedBitBoard) {
-    sq64 = POP(playedBitBoard);
-    printf("popped:%d\n", sq64);
-  }
 
   return 0;
 }
